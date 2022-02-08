0955a308c4ccd6abd3f4b6362f6d7aaa8fb2ecfc
Punit Naik <naik.punit44@gmail.com>
2022-02-09 00:27:59 +0530

    updated license name

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.punit-naik/in-memory-sql "0.1.0"
   :description "A Clojure library designed to work with an in-memory version of SQL DB for testing purposes."
   :url "https://github.com/punit-naik/in-memory-sql"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+  :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/java.jdbc "0.7.12"]
