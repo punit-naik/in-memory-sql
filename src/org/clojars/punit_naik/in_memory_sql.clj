00e40782cc8dc4a4e1a13657c7a5cae83cff314c
Punit Naik <naik.punit44@gmail.com>
2022-02-09 01:15:44 +0530

    added start and stop fns

--- src/org/clojars/punit_naik/in_memory_sql.clj
+++ src/org/clojars/punit_naik/in_memory_sql.clj
@@ -1,6 +1,25 @@
-(ns org.clojars.punit-naik.in-memory-sql)
+(ns org.clojars.punit-naik.in-memory-sql
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn start
+  "Starts up the DB and optionally runs `setup-ddl-statements` defined as string in a vector or a string separated by `\n`
+   Returns the `db` object"
+  [& setup-ddl-statements]
+  (let [db (->> "jdbc:sqlite::memory:"
+                (assoc {} :connection-uri)
+                jdbc/get-connection
+                (assoc {} :connection))]
+    (->> setup-ddl-statements
+         (mapcat
+          (fn [statement]
+            (if (str/includes? statement "\n")
+              (->> (str/split statement #"\n")
+                   (filter seq))
+              (list statement))))
+         (jdbc/db-do-commands db))
+    db))
+
+(defn stop
+  [db]
+  (-> db :connection .close))
