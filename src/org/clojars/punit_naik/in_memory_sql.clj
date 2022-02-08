4b72a77ac21f6a6685ebbb252583b0e3845171ce
Punit Naik <naik.punit44@gmail.com>
2022-02-08 23:03:10 +0530

    🎁 Init - set up repo

---
+++ src/org/clojars/punit_naik/in_memory_sql.clj
@@ -0,0 +1,6 @@
+(ns org.clojars.punit-naik.in-memory-sql)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/org/clojars/punit_naik/in_memory_sql_test.clj
@@ -0,0 +1,7 @@
+(ns org.clojars.punit-naik.in-memory-sql-test
+  (:require [clojure.test :refer :all]
+            [org.clojars.punit-naik.in-memory-sql :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
