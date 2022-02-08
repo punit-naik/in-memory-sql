e617fc6d663776d7ee655aefa6d4fa5fdc0f1a5b
Punit Naik <naik.punit44@gmail.com>
2022-02-09 00:28:13 +0530

    added component dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/punit-naik/in-memory-sql"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[com.stuartsierra/component "1.0.0"]
+                 [org.clojure/clojure "1.10.3"]
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.xerial/sqlite-jdbc "3.36.0.3"]]
   :repl-options {:init-ns org.clojars.punit-naik.in-memory-sql})
